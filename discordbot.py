8de61e3618a847214daa61c856843b39aa710424
yagu916 <51894848+yagu916@users.noreply.github.com>
2019-06-17 11:41:44 +0900

    add.neko.yosi

--- discordbot.py
+++ discordbot.py
@@ -14,14 +14,14 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
+    
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-        # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-    await message.channel.send('にゃーん')
+            # 「/neko」と発言したら「にゃーん」が返る処理
+async def neko(ctx):
+    await ctx.send('ヨシ！')
 
 bot.run(token)
