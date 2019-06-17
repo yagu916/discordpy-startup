89dc69963d365c70eff41e70f1ea8e53db5ef1e1
yagu916 <51894848+yagu916@users.noreply.github.com>
2019-06-17 11:28:05 +0900

    modosu

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,7 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
@@ -13,7 +14,6 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-    
+
 
 bot.run(token)
