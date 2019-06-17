5c116b2d8d1066559c9eba2c342bbb87c5cb4f45
yagu916 <51894848+yagu916@users.noreply.github.com>
2019-06-17 11:18:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,8 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-client = discord.Client()
-
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -17,15 +15,5 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
-    @client.event
-async def on_ready():
-  print("logged in as " + client.user.name)
-
-@client.event
-async def on_message(message):
-  if message.author != client.user:
-    msg = message.author.mention + " Hi."
-    await client.send_message(message.channel, msg)
-
 
 bot.run(token)
