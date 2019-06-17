b44ca2688d86408866c69e00acfbbb2a31a7ae8a
yagu916 <51894848+yagu916@users.noreply.github.com>
2019-06-17 11:36:25 +0900

    add.message.neko

--- discordbot.py
+++ discordbot.py
@@ -9,6 +9,14 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
+    
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 
 @bot.command()
